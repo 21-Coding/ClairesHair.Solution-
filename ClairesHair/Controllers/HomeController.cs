eed864569835f8b3f8ce11140eef0e86990d69af
DominiqueYoumans <ddyoumans@icloud.com>
2020-03-21 10:54:10 -0700

    update home controller.

---
+++ ClairesHair/Controllers/HomeController.cs
@@ -0,0 +1,15 @@
+using Microsoft.AspNetCore.Mvc;
+
+namespace Claire.Controllers
+{
+    public class HomeController : Controller
+    {
+    
+        [HttpGet("/")]
+        public ActionResult Index()
+            {
+                return View();
+            }
+
+    }   
+}
